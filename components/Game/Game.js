194c20ccb84fe56151a719e9a776b3b4b38fbd55
ecancino <eduardo.cancino@ciudadde.mx>
2020-08-04 15:43:14 -0400

    1 file added

---
+++ components/Game/Game.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from "react";
+
+import reducer from "./reducer";
+import initialState from "./state";
+
+import Grid from "./Grid";
+
+export default function Game() {
+  const [state, dispatch] = useReducer(reducer, initialState());
+  const { grid, status, turn } = state;
+
+  const handleClick = (x, y) => {
+    dispatch({ type: "CLICK", payload: { x, y } });
+  };
+
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
+
+  return (
+    <div css={{ display: "inline-block" }}>
+      <div
+        css={{
+          display: "flex",
+          justifyContent: "space-between"
+        }}
+      >
+        <div>Next turn: {turn}</div>
+        <div>{status === "success" ? `${turn} won!` : null}</div>
+        <button onClick={reset} type="button">
+          reset
+        </button>
+      </div>
+      <Grid grid={grid} handleClick={handleClick} />
+    </div>
+  );
+}
