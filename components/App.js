f8e06e27d3bd12e036db81d5d243539e11aeb7b3
ecancino <eduardo.cancino@ciudadde.mx>
2020-08-04 16:13:51 -0400

    2 files modified, 1 added

--- components/App.js
+++ components/App.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import Game from "./Game";
 
-export default function App() {
-  return (
-    <div>
-      <header>
-        <h1>Tic Tac Toe in React</h1>
-      </header>
-      <Game />
-    </div>
-  );
-}
+export default () => (
+  <div>
+    <header>
+      <h1>Tic Tac Toe in React</h1>
+    </header>
+    <Game />
+  </div>
+);
--- components/Cell.js
+++ components/Cell.js
@@ -2,26 +2,24 @@ import React from "react";
 
 import { COLOR } from "./state";
 
-export default function Cell({ onClick, value }) {
-  return (
-    <button
-      style={{
-        border: "none",
-        outline: "none",
-        fontSize: "5rem",
-        width: 100,
-        height: 100,
-        color: COLOR[value],
-        backgroundColor: "#fff",
-        "&:hover": {
-          color: "red"
-        }
-      }}
-      onClick={onClick}
-      type="button"
-      disabled={value}
-    >
-      {value}
-    </button>
-  );
-}
+export default ({ onClick, value }) => (
+  <button
+    style={{
+      border: "none",
+      outline: "none",
+      fontSize: "5rem",
+      width: 100,
+      height: 100,
+      color: COLOR[value],
+      backgroundColor: "#fff",
+      "&:hover": {
+        color: "red"
+      }
+    }}
+    onClick={onClick}
+    type="button"
+    disabled={value}
+  >
+    {value}
+  </button>
+);
